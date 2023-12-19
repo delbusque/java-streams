f2c08720089e6c015132036f431a533e68d59b29
delbusque <del.busque@gmail.com>
2023-12-19 13:34:21 +0200

    Update GettingStarted.java

--- src/test/java/com/amigoscode/examples/GettingStarted.java
+++ src/test/java/com/amigoscode/examples/GettingStarted.java
@@ -22,5 +22,7 @@ public class GettingStarted {
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        List<Person> youngPeople = people.stream().filter(p -> p.getAge() <= 18).limit(10).toList();
+        youngPeople.forEach(System.out::println);
     }
 }
