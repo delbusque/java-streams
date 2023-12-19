2db126e6ec9b7fefdd710561bc2cb4880532a2a8
delbusque <del.busque@gmail.com>
2023-12-19 13:34:24 +0200

    Update TransformationsMapAndReduce.java

--- src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -12,11 +12,25 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class TransformationsMapAndReduce {
 
     @Test
     void yourFirstTransformationWithMap() throws IOException {
         List<Person> people = MockData.getPeople();
+        List<PersonDTO> dtos = people.stream().map(p -> new PersonDTO(p.getId(), p.getFirstName(), p.getAge()))
+                .filter(p -> p.getAge() < 10)
+                .collect(Collectors.toList());
+
+        List<PersonDTO> dtosStatic = people.stream().map(p -> PersonDTO.map(p))
+                .filter(p -> p.getAge() > 60)
+                .collect(Collectors.toList());
+
+        dtos.forEach(System.out::println);
+        dtosStatic.forEach(System.out::println);
+
+        assertThat(people.size()).isNotEqualTo(dtosStatic.size());
     }
 
     @Test
